7061d5fc27a231e79d39f416e718ed04fad2c7d2
Prashant <87175096+shant-11@users.noreply.github.com>
2023-05-31 00:01:52 +0530

    finalise 01-...

--- 04-Range_Queries/01-Static_Range_Sum_Queries.cpp
+++ 04-Range_Queries/01-Static_Range_Sum_Queries.cpp
@@ -1,1 +1,63 @@
-bkdjk
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+#define ar array
+#define vt vector
+#define pb push_back
+
+#define F_OR(i, a, b, s) for (int i=(a); (s)>0?i<(b):i>(b); i+=(s))
+#define EACH(x, a) for (auto& x:a)
+
+template<class T> void read(T& x){
+    cin>>x;
+}
+template<class H, class... T> void read(H& h, T&... t) {
+	read(h);
+	read(t...);
+}
+template<class A> void read(vt<A>& x){
+    EACH(a, x)
+      read(a);
+}
+template<class A, size_t S> void read(ar<A, S>& x) {
+	EACH(a, x)
+		read(a);
+}
+
+const ll md=1000000007;
+const int d4i[4]={-1, 0, 1, 0}, d4j[4] = {0, 1, 0, -1};
+
+const int mxN = 2e5+1;
+ll x[mxN], sum[mxN];
+void solve(){
+    int n,q; read(n,q);
+    sum[0]=0;
+    F_OR(i,1,n+1,1){
+        cin>>x[i];
+        sum[i] = sum[i-1] + x[i];
+    }
+    F_OR(i,0,q,1){
+        int a,b; read(a,b);
+        cout<<(sum[b] - sum[a-1])<<'\n';
+    }
+}
+
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    
+    int t=1;
+   // read(t);
+    while(t--){
+        solve();
+    }
+    
+}
